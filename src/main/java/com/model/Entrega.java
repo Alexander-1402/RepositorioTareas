e34aa85e714d8406c11eff54133f15a0ab56101b
Rodrigo <rodrigoolguin8@gmail.com>
2026-04-13 19:25:26 -0400

    Entrega java mejora
    Added fields for nota, comentario, and id with corresponding getters and setters.

--- src/main/java/com/model/Entrega.java
+++ src/main/java/com/model/Entrega.java
@@ -7,13 +7,30 @@ public class Entrega {
     private Tarea tarea;
     private File archivo;
 
+    private static int c= 1;
+
+    private int nota;
+    private String comentario;
+     private int id;
     public Entrega(Alumno alumno, Tarea tarea, File archivo) {
         this.alumno = alumno;
         this.tarea = tarea;
         this.archivo = archivo;
+        this.id=c++;
+        this.nota = 0;
+        this.comentario = "";
     }
 
     public Alumno getAlumno() { return alumno; }
     public Tarea getTarea() { return tarea; }
     public File getArchivo() { return archivo; }
+
+    
+    public int getNota() { return nota; }
+    public void setNota(int nota) { this.nota = nota; }
+
+    public String getComentario() { return comentario; }
+    public void setComentario(String comentario) { this.comentario = comentario; }
+    public int getId() { return id; }
+public void setId(int id) { this.id = id; }
 }
